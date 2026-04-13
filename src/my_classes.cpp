8bce01cad84c2beaa8d3d3cac9c5f470fd182ad2
Erik Nordhamn <Erik.Nordhamn@rymdbob.se>
2026-04-13 17:24:31 +0200

    classes

---
+++ src/my_classes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Aclass {
+    public:
+        std::string name;
+        Aclass(std::string the_name) : name(the_name) {
+            std::cout << "Aclass constructor run\n";
+        };
+        int change_name(std::string new_name) {
+            name = new_name;
+            return 0;
+        };
+};
+
+void func(int &x) {
+    x = 42;
+};
+
+int main() {
+    int *x = new int(0);  // allocate an int on the heap, initialized to 0
+    int y = 56;
+
+    Aclass a("my name");
+    std::cout << "a.name = " << a.name << "\n";
+    std::cout << "x = " << *x << "\n";
+    func(*x);  // pass the int by reference to func, which modifies it
+    func(y);   // pass y by reference to func, which modifies it
+    std::cout << "x = " << *x << "\n";
+    std::cout << "y = " << y << "\n";
+};
